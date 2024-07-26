6819ddba3b1a4e2accc92d1ad3f1bfc742b584cb
Wachirawit Tangdan <59642775+wachirawittd123@users.noreply.github.com>
2024-07-26 18:07:22 +0700

    Update common.jsx

--- src/auth/common.jsx
+++ src/auth/common.jsx
@@ -13,7 +13,7 @@ export const checkAuth = async(req) => {
     let token = req?.cookies?.token?.toString().length > 0 ? req?.cookies?.token?.replace("j:", "") : null
     if(!token) return redirectLogin()
     try {
-        const result = await axios.post("http://localhost:3000/api/user/check-token", {}, {
+        const result = await axios.post(`${process.env.API_URL || 'http://localhost:3000'}/api/user/check-token`, {}, {
             headers:{
                 Authorization: `Bearer ${token}`
             } 
