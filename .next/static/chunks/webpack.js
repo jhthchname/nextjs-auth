9029923e27c67dbd6cde9a51ef1ca530526b810d
Wachirawit Tangdan <59642775+wachirawittd123@users.noreply.github.com>
2024-07-26 18:16:15 +0700

    add APP_URL for .env

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "400bd1d46cb21f81"; }
+/******/ 		__webpack_require__.h = function() { return "74f7fed0e0c6267c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/auth/common.jsx
+++ src/auth/common.jsx
@@ -11,6 +11,7 @@ const redirectLogin = () => {
 
 export const checkAuth = async(req) => {
     let token = req?.cookies?.token?.toString().length > 0 ? req?.cookies?.token?.replace("j:", "") : null
+    console.log('process.env.API_URL=========>',process.env.API_URL)
     if(!token) return redirectLogin()
     try {
         const result = await axios.post(`${process.env.API_URL || 'http://localhost:3000'}/api/user/check-token`, {}, {
