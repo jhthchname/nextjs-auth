dab578133eaea878c49066bfbdf30da4da2f6975
Wachirawit Tangdan <59642775+wachirawittd123@users.noreply.github.com>
2024-07-26 16:04:34 +0700

    vercel.json fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9251240fdffabf9e"; }
+/******/ 		__webpack_require__.h = function() { return "400bd1d46cb21f81"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.bcbe92f12213433b.hot-update.js
+++ .next/static/webpack/webpack.bcbe92f12213433b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "4ff02eba22a93e9d"; }
+/******/ 	__webpack_require__.h = function() { return "05bcc51e933c070e"; }
 /******/ }();
 /******/ 
 /******/ }
